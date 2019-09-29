66218ea36152b240b39fd4995f8bf65610cb23ae
zhen.yu <zhen.yu@sim.com>
2019-09-29 17:37:56 +0800

    [加入创建数据库和表的各项配置]

---
+++ app/src/main/java/com/example/mairweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.mairweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countyName = countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
